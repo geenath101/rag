f989132bc3cc3d0e6109ea7dd0bfac1061b568e7
Geenath Perera <geenathdl024@gmail.com>
2026-01-18 19:49:19 +1300

    updated bm25search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -15,6 +15,27 @@ MOVIE_DATABASE=""
 BM25_K1=1.5
 BM25_B = 0.75
 
+
+def pre_process_str(moviname:str) -> list[str]:
+        stemmer = PorterStemmer()
+        mapping = str.maketrans("","",string.punctuation)
+        trans_str = moviname.lower().translate(mapping)
+        tokenized = trans_str.split()
+        return[ stemmer.stem(s) for s in tokenized if s not in STOP_WORD_LIST ]
+
+def check_for_match(query:list[str],title:list[str])->bool:
+    for q in query:
+        for k in title:
+            if q in k:
+                return True
+    return False
+
+def init_database():
+    json_file = open("data/movies.json")
+    val = json.load(json_file)
+    return val["movies"]
+
+
 class InvertedIndex:
     def __init__(self):
         self.index = collections.defaultdict(set)
@@ -64,9 +85,6 @@ class InvertedIndex:
         return self.tern_frequencies.get(doc_id)[term]
     
     def get_bm25_idf(self, term: str) -> float:
-        self.index = self.load("cache/index.pkl")
-        self.docmap = self.load("cache/docmap.pkl")
-        self.tern_frequencies = self.load("cache/term_frequencies.pkl")
         n = len(self.docmap)
         df = len(self.index.get(term))
         return math.log((n - df + 0.5) / (df + 0.5) + 1)
@@ -77,7 +95,30 @@ class InvertedIndex:
         #for key,val in self.doc_length:
         #    total = sum (val)
         return total/len(self.doc_length)
+
+
+    def get_bm2f(self,doc_id,term):
+        tfs = self.get_bm2f_tf(doc_id,term)
+        # no need to calculate idf for each doc
+        idf = self.get_bm25_idf(term)
+        return tfs * idf
     
+    def bm25_search(self, query, limit):
+        tq = pre_process_str(query)
+        scores = {}
+        for q in tq:
+            _idf = self.get_bm25_idf(q)
+            for d in self.index[q]:
+                sc = _idf * self.get_bm2f_tf(d,q)
+                if d not in scores:
+                    scores[d] = sc
+                else:
+                    print(f"accumilating already existing id ")
+                    scores[d] = scores.get(d) + sc
+        sorted_scores = sorted(scores.items(),key=lambda item:item[1])
+        return sorted_scores[:4]
+        
+
     
     def build(self):
         for m in MOVIE_DATABASE:
@@ -101,25 +142,6 @@ class InvertedIndex:
         return doc_id, tokens
     
 
-def pre_process_str(moviname:str) -> list[str]:
-        stemmer = PorterStemmer()
-        mapping = str.maketrans("","",string.punctuation)
-        trans_str = moviname.lower().translate(mapping)
-        tokenized = trans_str.split()
-        return[ stemmer.stem(s) for s in tokenized if s not in STOP_WORD_LIST ]
-
-def check_for_match(query:list[str],title:list[str])->bool:
-    for q in query:
-        for k in title:
-            if q in k:
-                return True
-    return False
-
-def init_database():
-    json_file = open("data/movies.json")
-    val = json.load(json_file)
-    return val["movies"]
-
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI run simpel search by uv run cli/keyword_search_cli.py search 'movie name'")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -141,13 +163,16 @@ def main() -> None:
 
     bm25idf_parser = subparsers.add_parser("bm25idf", help="return bm25idf value")
     bm25idf_parser.add_argument("term",type=str,help="term")
-
+    
     bm25_tf_parser = subparsers.add_parser(
     "bm25tf", help="Get BM25 TF score for a given document ID and term"
     )
     bm25_tf_parser.add_argument("doc_id", type=int, help="Document ID")
     bm25_tf_parser.add_argument("term", type=str, help="Term to get BM25 TF score for")
     bm25_tf_parser.add_argument("k1", type=float, nargs='?', default=BM25_K1, help="Tunable BM25 K1 parameter")
+
+    bm25search_parser = subparsers.add_parser("bm25search", help="Search movies using bm25search")
+    bm25search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
 
@@ -212,6 +237,13 @@ def main() -> None:
             term = pre_process_str(args.term)
             bm25tf = i.get_bm2f_tf(args.doc_id,term[0])
             print(f"BM25 TF score of '{args.term}' in document '{args.doc_id}': {bm25tf:.2f}")
+        case "bm25search":
+            i = InvertedIndex()
+            i.index = i.load("cache/index.pkl")
+            i.tern_frequencies = i.load("cache/term_frequencies.pkl")
+            i.doc_length = i.load("cache/doc_length.pkl")
+            sorted_scores = i.bm25_search(args.query,5)
+            print(f"sorted_scores {sorted_scores}")
         case _:
             parser.print_help()
 
